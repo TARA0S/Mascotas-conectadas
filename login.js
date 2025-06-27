c0fa657fa496d8e6212b41f873f2f36de0f80fcd
Ignacio A. Laguna Cabrera <nachitolaguna06@gmail.com>
2025-06-27 11:38:51 -0300

    Eliminando archivos innecesarios

--- login.js
+++
@@ -1,5 +0,0 @@
-//en este sitio web esta la info de como ocultar o mostrar 
-//cada pantalla: https://docs.idew.org/code-internet-of-things/references/web-app-multiple-screens
-// _apretar el "olvidaste usuario y contraseña" redirige a screen 2 y luego a screen 5
-// _apretar el "no tienes una cuenta" redirige a screen 3 y luego a screen 4
-//gracias 👍.
